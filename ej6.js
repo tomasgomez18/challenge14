9b1acfdc4322edb4c8179e59fe87c253e5c4aa92
tomas <tomasgomezzi36@gmail.com>
2025-08-27 09:15:50 -0300

    add exercises

---
+++ ej6.js
@@ -0,0 +1,12 @@
+/* Crea una función que reciba un array y una posición, y elimine el elemento con .splice(). */
+let entrada = prompt("Ingrese un elemento para agregar.");
+let arrayDef = [1, 2, 3, 4, 5, 6];
+
+function eliminarPosicion (arrayDef){
+arrayDef.splice(entrada)
+return arrayDef;    
+}
+
+let resultado = eliminarPosicion (arrayDef);
+console.log(eliminarPosicion (arrayDef));
+    
